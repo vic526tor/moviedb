0f896b4cd941019307c351815a17f7477b27139a
vic526tor <vic526tor@gmail.com>
2020-07-18 22:06:17 +0800

    first commit

---
+++ EndingListener.java
@@ -0,0 +1,8 @@
+import java.awt.event.ActionListener;
+//This is the file EndingListener.java .
+import java.awt.event.ActionEvent;
+public class EndingListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        System.exit(0);
+    }
+}
